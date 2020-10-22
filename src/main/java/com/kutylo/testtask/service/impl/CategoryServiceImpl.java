bfc018f16cce1733f063589ed306260a223160fa
Roman Kutylo <potteroman0@gmail.com>
2020-10-22 16:50:51 +0300

    Implementation product and category service methods

--- src/main/java/com/kutylo/testtask/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/kutylo/testtask/service/impl/CategoryServiceImpl.java
@@ -2,45 +2,78 @@ package com.kutylo.testtask.service.impl;
 
 import com.kutylo.testtask.dto.request.CategoryRequest;
 import com.kutylo.testtask.dto.response.CategoryResponse;
+import com.kutylo.testtask.mapper.CategoryMapper;
 import com.kutylo.testtask.mapper.ProductMapper;
+import com.kutylo.testtask.model.Category;
+import com.kutylo.testtask.model.Product;
+import com.kutylo.testtask.repository.CategoryRepository;
 import com.kutylo.testtask.repository.ProductRepository;
 import com.kutylo.testtask.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class CategoryServiceImpl implements CategoryService {
 
-    private final ProductRepository productRepository;
-    private final ProductMapper productMapper;
+    private final CategoryRepository categoryRepository;
+    private final CategoryMapper categoryMapper;
 
     @Override
     public List<CategoryResponse> findAllCategories() {
-        return null;
+
+        log.info("Getting list of all categories");
+
+        return categoryRepository.findAll().stream()
+                .map(categoryMapper::modelToResponse)
+                .collect(Collectors.toList());
     }
 
     @Override
     public CategoryResponse findCategoryById(int id) {
-        return null;
+        log.info("Getting category by id:" + id);
+
+        Category category = categoryRepository.findById(id)
+                .orElseThrow(() -> {
+                    log.error("Not found category with id:" + id);
+                    return new EntityNotFoundException("Not found category with id:" + id);
+                });
+
+        return categoryMapper.modelToResponse(category);
     }
 
     @Override
     public CategoryResponse saveCategory(CategoryRequest categoryRequest) {
-        return null;
+        log.info("Saving category = " + categoryRequest);
+
+        Category category = categoryMapper.requestToModel(categoryRequest);
+        categoryRepository.save(category);
+
+        return categoryMapper.modelToResponse(category);
     }
 
     @Override
     public CategoryResponse updateCategoryById(int id, CategoryRequest categoryRequest) {
-        return null;
+        log.info("Updating category with id:" + id);
+
+        Category category = categoryMapper.requestToModel(categoryRequest);
+        category.setId(id);
+        categoryRepository.save(category);
+
+        return categoryMapper.modelToResponse(category);
     }
 
     @Override
     public void deleteCategoryById(int id) {
+        log.info("Deleting category with id:" + id);
+
+        categoryRepository.deleteById(id);
 
     }
 }
--- src/main/java/com/kutylo/testtask/service/impl/ProductServiceImpl.java
+++ src/main/java/com/kutylo/testtask/service/impl/ProductServiceImpl.java
@@ -2,33 +2,74 @@ package com.kutylo.testtask.service.impl;
 
 import com.kutylo.testtask.dto.request.ProductRequest;
 import com.kutylo.testtask.dto.response.ProductResponse;
+import com.kutylo.testtask.mapper.ProductMapper;
+import com.kutylo.testtask.model.Product;
+import com.kutylo.testtask.repository.ProductRepository;
 import com.kutylo.testtask.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository productRepository;
+    private final ProductMapper productMapper;
+
     @Override
     public List<ProductResponse> findAllProducts() {
-        return null;
+        log.info("Getting list of all products");
+
+        return productRepository.findAll().stream()
+                .map(productMapper::modelToResponse)
+                .collect(Collectors.toList());
+
     }
 
     @Override
     public ProductResponse findProductById(int id) {
-        return null;
+        log.info("Getting product by id:" + id);
+
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> {
+                    log.error("Not found product with id:" + id);
+                    return new EntityNotFoundException("Not found product with id:" + id);
+                });
+
+        return productMapper.modelToResponse(product);
     }
 
     @Override
     public ProductResponse saveProduct(ProductRequest productRequest) {
-        return null;
+        log.info("Saving product = " + productRequest);
+
+        Product product = productMapper.requestToModel(productRequest);
+        productRepository.save(product);
+
+        return productMapper.modelToResponse(product);
     }
 
     @Override
     public ProductResponse updateProduct(ProductRequest productRequest, int id) {
-        return null;
+        log.info("Update product with id: " + id);
+
+        Product product = productMapper.requestToModel(productRequest);
+        product.setId(id);
+        productRepository.save(product);
+
+        return productMapper.modelToResponse(product);
     }
 
     @Override
     public void deleteProduct(int id) {
+        log.info("Deleting product with id = " + id);
 
+        productRepository.deleteById(id);
     }
 }
